64b256c0812a14de94c914958d213fd04ffe0ac9
Aman Manandhar <amansenpai01@gmail.com>
2026-06-29 17:41:13 +0545

    dom manipulation

---
+++ change_text/change.js
@@ -0,0 +1,6 @@
+const title = document.getElementById("title");
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', function() {
+    title.innerText = 'Welcome User';
+});
